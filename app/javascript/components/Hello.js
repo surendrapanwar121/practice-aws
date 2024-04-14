5f1b3f98a417b558c6f3c6ebc3fdec0f288a0883
Surendra Panwar <surendrapanwar121@gmail.com>
2024-04-15 00:35:20 +0530

    Initial set for App

---
+++ app/javascript/components/Hello.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+import './Hello.scss'
+
+const propTypes = {
+  title: PropTypes.string,
+  body: PropTypes.string
+}
+
+const Hello = ({title, body}) => {
+  const [name, setName] = useState('Surendra Panwar')
+  const handleChange = (e) => {
+    if (!e.target.value.length) setName('Surendra Panwar')
+    else setName(e.target.value)
+  }
+  return (
+    <div className='container'>
+      <input onChange={handleChange} placeholder='Type your name here' />
+      <div>Name: {name}</div>
+      <div>Title: {title}</div>
+      <div>Body: {body}</div>
+    </div>
+  )
+}
+
+Hello.propTypes = propTypes
+export default Hello
