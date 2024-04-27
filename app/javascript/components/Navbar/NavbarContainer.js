f858fd31662d7a3fa880ad2603cc2d48e9df1bec
Surendra Panwar <surendrapanwar121@gmail.com>
2024-04-27 12:17:01 +0530

    Navbar from bootstrap

---
+++ app/javascript/components/Navbar/NavbarContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './Header'
+
+const propTypes = {
+}
+
+const NavbarContainer = () => {
+  return (
+    <Header/>
+  )
+}
+
+NavbarContainer.propTypes = propTypes
+export default NavbarContainer
